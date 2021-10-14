5334214c81994437928aa2a7b109f255933cf065
tomohito-tajima <tomo322017@gmail.com>
2021-10-14 09:02:03 +0000

    Finish 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '60ab09ee9584e914d570d040d35949aac39702d7c1485e2fba1f7e1451346bc3eda49c2ce1d397fb28291b62d565bbc1590bf5b5c1e00b971fd99c5ab60148d2'
